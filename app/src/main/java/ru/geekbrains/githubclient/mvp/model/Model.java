c5e40e8498e74edee9163b979fffa94a72d1f00f
Vladimir Alekseev <headgrass@yandex.ru>
2020-11-05 10:11:07 +0700

    Lesson1

---
+++ app/src/main/java/ru/geekbrains/githubclient/mvp/model/Model.java
@@ -0,0 +1,30 @@
+package ru.geekbrains.githubclient.mvp.model;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Model {
+    private List<Integer> counters = new ArrayList<>(Arrays.asList(0, 0, 0));
+
+    public int getCurrent(int index) {
+        // TODO: входной контроль параметров
+        return counters.get(index);
+    }
+    public void set(int index, int value) {
+        counters.set(index, value);
+    }
+
+    /** такая аннотация вызывается вводом слэш, две звезды Ввод.
+     * Инкремент счетчика
+     * @param index - штдекс счетчика
+     * @return инкрементированное значение
+     */
+    public  int next(int index) {
+        Integer nextValue = counters.get(index) + 1;
+
+        counters.set(index, nextValue);
+
+        return nextValue;
+    }
+}
